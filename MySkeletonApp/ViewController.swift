44309487f34eccf7d1d9dcd6fefae50fb3ea189e
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-03-01 09:01:15 +0900

    Add Skeleton View

--- MySkeletonApp/ViewController.swift
+++ MySkeletonApp/ViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import SkeletonView
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, SkeletonTableViewDataSource {
     
     @IBOutlet var tableView: UITableView!
     
@@ -25,12 +26,25 @@ class ViewController: UIViewController, UITableViewDataSource {
                 self.data.append("Some text")
             }
             
+            self.tableView.stopSkeletonAnimation()
+            self.view.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(0.25))
+            
             self.tableView.reloadData()
         })
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.isSkeletonable = true
+        tableView.showAnimatedGradientSkeleton(usingGradient: .init(baseColor: .concrete),animation: nil, transition: .crossDissolve(0.25))
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        return MyTableViewCell.identifier
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
